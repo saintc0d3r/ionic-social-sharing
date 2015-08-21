a95560b57298e8ce84faab78c631705e26ea3e97
Wendy Sanarwanto <saintc0d3r@gmail.com>
2015-08-21 09:43:37 +0800

    Refactored app.js by moving out the controller part into a separated file.

--- platforms/ios/social_sharing/main.m
+++ platforms/ios/social_sharing/main.m
@@ -18,7 +18,7 @@
  */
 //
 //  main.m
-//  HelloCordova
+//  social_sharing
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
